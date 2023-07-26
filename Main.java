ac135dc4aed6241a6dd547c154c9634d10d92fe6
rkozyak <rkozyak@gmail.com>
2023-07-26 09:20:26 -0400

    Added text below

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.StringConverter;
 
@@ -27,7 +27,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         // Create the label for the choice box
-        Label industryLabel = new Label("Industry:");
+        Text industryLabel = new Text("Select an Industry:");
 
         // Create the choice box for company group selection
         ChoiceBox<String> groupChoiceBox = new ChoiceBox<>();
@@ -38,7 +38,7 @@ public class Main extends Application {
         NumberAxis xAxis = new NumberAxis(1998, 2022, 1);
         NumberAxis yAxis = new NumberAxis();
         LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
-        lineChart.setTitle("Stock Data");
+        lineChart.setTitle("Historical Stock Performance");
         lineChart.setCreateSymbols(false);
 
         // Custom formatter for the x-axis to display years as integers
@@ -54,21 +54,43 @@ public class Main extends Application {
             }
         });
 
-        // Add the choice box listener to update the line chart when a group is selected
+        // Create a Text node for displaying the description text
+        Text descriptionText = new Text();
+        descriptionText.setWrappingWidth(800); // Adjust the width as needed
+
+        // Add the choice box listener to update the line chart and description text when a group is selected
         groupChoiceBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             int selectedGroupIndex = groupChoiceBox.getSelectionModel().getSelectedIndex();
             updateLineChart(lineChart, companyData[selectedGroupIndex], stockData[selectedGroupIndex]);
+
+            // Update the description text based on the selected group
+            String description = getDescriptionForGroup(selectedGroupIndex);
+            descriptionText.setText(description);
         });
 
-        // Create the main layout and add the label, choice box, and line chart to it
+        // Create the main layout and add the label, choice box, line chart, and description text to it
         VBox root = new VBox(10);
-        root.getChildren().addAll(industryLabel, groupChoiceBox, lineChart);
+        root.setPadding(new javafx.geometry.Insets(10));
+        root.getChildren().addAll(industryLabel, groupChoiceBox, lineChart, descriptionText);
 
         // Set up the scene and show the stage
         Scene scene = new Scene(root, 800, 600);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Stock Data Viewer");
+        primaryStage.setTitle("Un-Essay Project");
         primaryStage.show();
+    }
+
+    // Helper method to retrieve the description for each group (you can modify the descriptions as needed)
+    private String getDescriptionForGroup(int groupIndex) {
+        String[] descriptions = {
+            "Tech desciption: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "Bank Description",
+            "Aviation Description",
+            "Department store description",
+            "Oil Descritpion",
+            "Food Description"
+        };
+        return descriptions[groupIndex];
     }
 
     private void updateLineChart(LineChart<Number, Number> lineChart, String[] companyNames, double[][] stockData) {
